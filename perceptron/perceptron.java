7000b048737a42a9f2692d078bb93892be4d2a98
IshaqJunejo <ishaquejunejo33@gmail.com>
2025-01-26 22:08:52 +0500

    Training and Testing works, Accuracy almost 85%

--- perceptron/perceptron.java
+++ perceptron/perceptron.java
@@ -1,6 +1,6 @@
 class perceptron {
-	// Method to shuffle the dataset
-	private int[][] shuffleData(int[][] dataset) {
+	// Method to shuffle the dataset (risks losing some data due to duplication)
+	private static int[][] shuffleData(int[][] dataset) {
 		int[][] shuffledDataset = new int[dataset.length][dataset[0].length];
 		int randomizer;
 
@@ -12,41 +12,100 @@ class perceptron {
 		return shuffledDataset;
 	}
 
+	// Main Function
 	public static void main(String[] args) {
 		int length = 1000;
 		int size = 16;
 		// Dataset, Inputs, and Outputs
 		int[][] dataset = new int[length][size * size + 1];
-		double[][] inputs = new double[length][size * size];
-		double[] outputs = new double[length];
+		int[][] trainingData = new int[(int)(length * 0.75)][size * size + 1];
+		int[][] testData = new int[(int)(length * 0.25)][size * size + 1];
+		double[][] trainingInputs = new double[trainingData.length][size * size];
+		double[] trainingOutputs = new double[trainingData.length];
+		double[][] testInputs = new double[testData.length][size * size];
+		double[] testOutputs = new double[testData.length];
 
-		// Our only output neuron
+		// Our only neuron
 		neuron outputneuron = new neuron(size * size);
 		
 		// Reading the Dataset from a .txt file
 		dataset = dataReader.readTXT("dataset.txt", 1000, size * size + 1);
+		// Shuffling the Dataset
+		dataset = shuffleData(dataset);
 		
-		// Splitting the dataset into inputs and outputs (ie, Labels)
-		for (int i = 0; i < 1000; i++) {
-			for (int j = 0; j < size * size; j++) {
-				inputs[i][j] = (double)dataset[i][j];
+		// Splitting the dataset for training and testing
+		for (int i = 0; i < length; i++) {
+			if (i < trainingData.length) {
+				trainingData[i] = dataset[i];
+			} else {
+				testData[i - trainingData.length] = dataset[i];
 			}
-			outputs[i] = (double)dataset[i][size * size];
 		}
 
-		// Training for 200 Epochs
-		for (int i = 0; i < 200; i++) {
-			for (int j = 0; j < dataset.length; j++) {
-				// Forward Propagation
-				outputneuron.forwardPass(inputs[j]);
-				// Back Propagation (and updating parameters)
-				outputneuron.backPropagate(outputs[j], 0.1);
+		// Splitting the training dataset into inputs and outputs (ie, Labels)
+		for (int i = 0; i < trainingData.length; i++) {
+			for (int j = 0; j < size * size; j++) {
+				trainingInputs[i][j] = (double)trainingData[i][j];
 			}
+			trainingOutputs[i] = (double)trainingData[i][size * size];
+		}
+
+		// Splitting the testing dataset into inputs and outputs
+		for (int i = 0; i < testData.length; i++) {
+			for (int j = 0; j < size * size; j++) {
+				testInputs[i][j] = (double)testData[i][j];
+			}
+			testOutputs[i] = (double)testData[i][size * size];
+		}
+
+		// Training for 2000 Epochs
+		System.out.println("Delata 25 approves training for 2000 Epochs");
+		for (int i = 1; i <= 2000; i++) {
+			int correctPrediction = 0;
+			double accuracy = 0.0;
+			for (int j = 0; j < trainingData.length; j++) {
+				// Forward Propagation
+				outputneuron.forwardPass(trainingInputs[j]);
+				// Back Propagation (and updating parameters)
+				outputneuron.backPropagate(trainingOutputs[j], 0.1);
+
+				// Computing Accuracy
+				if (trainingOutputs[j] == 0 && outputneuron.getOutput() < 0.3) {
+					correctPrediction++;
+				} else if (trainingOutputs[j] == 1 && outputneuron.getOutput() > 0.7) {
+					correctPrediction++;
+				}
+			}
+
+			accuracy = (double)correctPrediction / (double)dataset.length;
 
 			// Update after every 10 Epoch
 			if (i % 10 == 0) {
 				System.out.println("Epoch No: " + i);
+				System.out.println("Accuracy: " + (accuracy * 100.0) + "%");
+				System.out.println();
 			}
 		}
+
+		// Testing
+		System.out.println("Delta 25 approves to test the Perceptron");
+		int correctPrediction = 0;
+		double accuracy = 0.0;
+		for (int i = 0; i < testData.length; i++) {
+			// Forward Propagation
+			outputneuron.forwardPass(testInputs[i]);
+
+			// Counting Correct Predictions
+			if (testOutputs[i] == 0 && outputneuron.getOutput() < 0.3) {
+				correctPrediction++;
+			} else if (testOutputs[i] == 1 && outputneuron.getOutput() > 0.7) {
+				correctPrediction++;
+			}
+		}
+		// Calculating Accuracy
+		accuracy = (double)correctPrediction / (double)testData.length;
+
+		//
+		System.out.println("Accuracy in the test: " + (accuracy * 100) + "%");
 	}
 }
